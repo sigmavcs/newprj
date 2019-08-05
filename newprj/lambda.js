6511ef79cdb5b6607610115c7dcb246dfddd4fb3
sigmavcs <jchamath+sigma@gmail.com>
2019-08-05 12:45:28 +0530

    s1

--- newprj/lambda.js
+++ newprj/lambda.js
@@ -1,4 +1,21 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+
+    sns.publish({
+        Message: 'Message',
+        Subject: 'Subject',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:my_topic'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
--- newprj/sub/mypy.py
+++ newprj/sub/mypy.py
@@ -1,3 +1,15 @@
+import boto3
+ddb = boto3.client("dynamodb")
+
 def handler(event, context):
     
-    return {"message": "Successfully executed"}
+    try:
+        data = ddb.get_item(
+            TableName='BTMenu',
+            Key={'itemCode':{'S':'name'}}
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
+
+    return {"message": "Successfully executed!!!"}
